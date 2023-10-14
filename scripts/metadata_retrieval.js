8f57af5fdfffcd0564be748a24a3573603c80257
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2023-10-14 15:00:38 +0530

    removed metadata_retrieval.js

--- scripts/metadata_retrieval.js
+++
@@ -1,8 +0,0 @@
-musicPlayer.addEventListener('loadedmetadata', () => 
-{
-    const artist = musicPlayer.getAttribute('artist');
-    const title = musicPlayer.getAttribute('title');
-    const album = musicPlayer.getAttribute('album');
-
-    return(artist, title, album);
-});
