136d2de07bde88b70424dbb127f49fb42e2f95f2
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2023-10-27 03:50:21 +0530

    added functionality to change gradient colors using color-thief.js

--- scripts/file_loader.js
+++ scripts/file_loader.js
@@ -1,3 +1,5 @@
+// import Vibrant from 'node-vibrant';
+
 const blocksButton = document.getElementById("blocksButton");
 const audioFileLoader = document.createElement("input");
 const audioPlayer = document.querySelector(".audio-player");
@@ -44,8 +46,29 @@ audioFileLoader.addEventListener("change", function()
                     }
                     else
                     {
-                        coverArt.src = "public/default.jpg";
+                        coverArt.src = "public/default.jpg"; //doesn't work to set to default image
                     }
+
+                    const sections = document.querySelectorAll("section");
+                    const colorThief = new ColorThief();
+
+                    coverArt.onload = function ()
+                    {
+                        const colorPalette = colorThief.getPalette(coverArt, 12);
+                        console.log(colorPalette);
+
+                        sections.forEach((section, index) => 
+                        {
+                            const color1 = colorPalette[2 * index];
+                            const color2 = colorPalette[2 * index + 1];
+                            section.style.background = `linear-gradient(75deg, rgb(${color1[0]}, ${color1[1]}, ${color1[2]}), rgba(${color2[0]}, ${color2[1]}, ${color2[2]}, 0.4))`;
+                        });
+
+                        const body = document.querySelector(".container");
+                        body.style.background = `linear-gradient(75deg, rgb(${colorPalette[0][0]}, ${colorPalette[0][1]}, ${colorPalette[0][2]}), rgb(${colorPalette[5][0]}, ${colorPalette[5][1]}, ${colorPalette[5][2]})`;
+                    };
+
+
                 }
                 else
                 {
